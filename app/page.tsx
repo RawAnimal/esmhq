899909fc99e8ee0996e3d765c4853e9d6fe364f5
Sean Sweeney <skjsweeney@gmail.com>
2023-11-03 09:28:15 -0400

    Added NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-import {
-  MaterialReactTable,
-  useMaterialReactTable,
-} from 'material-react-table';
+import NavBar from './_components/NavBar';
 
 export default function Home() {
-  const data = [
-    {
-      name: 'John', // key "name" matches `accessorKey` in ColumnDef down below
-      age: 30, // key "age" matches `accessorKey` in ColumnDef down below
-    },
-    {
-      name: 'Sara',
-      age: 25,
-    },
-  ];
-
-  return <div>Main Page</div>;
+  return (
+    <div>
+      <NavBar />
+      <h1>Main Page</h1>
+    </div>
+  );
 }
