0bf7ae943c1565b1b6781072610a020513a17680
Sean Sweeney <skjsweeney@gmail.com>
2023-11-04 09:27:23 -0400

    Added new site button

--- app/sites/page.tsx
+++ app/sites/page.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import SitesTable from '@/app/sites/_components/SitesTable';
 import NavBar from '../_components/NavBar';
-import { Container } from '@mui/material';
+import { Box, Button, Container } from '@mui/material';
+import Link from 'next/link';
 
 const SitesPage = () => {
   return (
     <>
       <NavBar />
       <Container sx={{ pt: 2 }}>
+        <Box>
+          <Button>
+            <Link href="/sites/new">New</Link>
+          </Button>
+        </Box>
         <SitesTable />
       </Container>
     </>
