1df47381766eba2e04e97e53fa88883ac8887e4a
Sean Sweeney <skjsweeney@gmail.com>
2023-11-07 15:14:08 -0500

    Handle errors

--- app/sites/_components/DeleteSiteButton.tsx
+++ app/sites/_components/DeleteSiteButton.tsx
@@ -13,28 +13,33 @@ import { useState } from 'react';
 
 const DeleteSiteButton = ({ siteId }: { siteId: number }) => {
   const router = useRouter();
-  const [open, setOpen] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [openError, setOpenError] = useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
+  const handleClickOpenDialog = () => {
+    setOpenDialog(true);
+  };
+  const handleClickCloseDialog = () => {
+    setOpenDialog(false);
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const handleErrorClose = () => {
+    setOpenError(false);
   };
+
   return (
     <>
       <Button
         fullWidth
         variant="contained"
         color="warning"
-        onClick={handleClickOpen}
+        onClick={handleClickOpenDialog}
       >
         Delete
       </Button>
       <Dialog
-        open={open}
-        onClose={handleClose}
+        open={openDialog}
+        onClose={handleClickCloseDialog}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
@@ -46,21 +51,53 @@ const DeleteSiteButton = ({ siteId }: { siteId: number }) => {
           </DialogContentText>
         </DialogContent>
         <DialogActions sx={{ mb: 1, mr: 1 }}>
-          <Button onClick={handleClose} variant="contained" color="secondary">
+          <Button
+            onClick={handleClickCloseDialog}
+            variant="contained"
+            color="secondary"
+          >
             Cancel
           </Button>
           <Button
             onClick={async () => {
-              await axios.delete('/api/sites/' + siteId);
-              setOpen(false);
-              router.push('/sites');
-              router.refresh();
+              try {
+                setOpenDialog(false);
+                await axios.delete('/api/sites/' + siteId);
+                router.push('/sites');
+                router.refresh();
+              } catch (error) {
+                setOpenDialog(false);
+                setOpenError(true);
+              }
             }}
             autoFocus
             variant="contained"
             color="warning"
           >
             Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog
+        open={openError}
+        onClose={handleErrorClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{'Error'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            This site could not be deleted.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleErrorClose}
+            autoFocus
+            variant="contained"
+            color="secondary"
+          >
+            OK
           </Button>
         </DialogActions>
       </Dialog>
