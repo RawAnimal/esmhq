8a20d0dd96ec867d63515b3c6e2abb29e6fe9bea
Sean Sweeney <skjsweeney@gmail.com>
2023-11-11 13:33:59 -0500

    Updated Avatar to add color and initials

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -26,6 +26,35 @@ const pages = [
   { title: 'Sites', href: '/sites' },
 ];
 
+function stringToColor(string: string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name: string) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  };
+}
+
 function NavBar() {
   const pathname = usePathname();
   const { status, data: session } = useSession();
@@ -164,7 +193,12 @@ function NavBar() {
             </Box>
             <Box sx={{ flexGrow: 0 }}>
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" />
+                <Avatar
+                  //variant="rounded"
+                  {...stringAvatar(
+                    session?.user.firstName + ' ' + session?.user.lastName
+                  )}
+                />
               </IconButton>
               <Menu
                 sx={{ mt: '45px' }}
