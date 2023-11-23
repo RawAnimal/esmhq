0eab499d42f84ac2c5c33c0de415ad3b0338b135
Sean Sweeney <skjsweeney@gmail.com>
2023-11-23 15:34:02 -0500

    test fix bigint json issue in route

--- app/api/sites/stats/[year]/route.ts
+++ app/api/sites/stats/[year]/route.ts
@@ -10,13 +10,25 @@ interface Stats {
   f5: string; // total_est_hours
 }
 
+// BigInt.prototype.toJSON = function () {
+//   return this.toString();
+// };
+
 export async function GET(
   request: NextRequest,
   { params }: { params: { year: string } }
 ) {
   if (params.year === 'ALL') {
     const stats = await prisma.$queryRaw<Stats[]>`call getSitesData(null)`;
-    return NextResponse.json(stats);
+    const newStats = stats.map((obj) =>
+      Object.fromEntries(
+        Object.entries(obj).map(([key, val]) => [
+          key,
+          typeof val === 'bigint' ? Number(val) : val,
+        ])
+      )
+    );
+    return NextResponse.json(newStats);
   } else if (+params.year) {
     // +num returns the numeric value of the string, or NaN
     // if the string isn't purely numeric characters
@@ -27,7 +39,15 @@ export async function GET(
     if (stats.length == 0) {
       return NextResponse.json({ message: 'No results' });
     }
-    return NextResponse.json(stats);
+    const newStats = stats.map((obj) =>
+      Object.fromEntries(
+        Object.entries(obj).map(([key, val]) => [
+          key,
+          typeof val === 'bigint' ? Number(val) : val,
+        ])
+      )
+    );
+    return NextResponse.json(newStats);
   } else {
     return NextResponse.json({ error: 'Invalid year' }, { status: 400 });
   }
